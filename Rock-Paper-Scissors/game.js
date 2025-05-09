0ad6f1aee2b766d584615a7613cdbfc923d869e9
PhamNhatQuang207 <phamquang207207@gmail.com>
2025-05-09 10:31:00 +0700

    Rock Paper Scissors Game

---
+++ Rock-Paper-Scissors/game.js
@@ -0,0 +1,38 @@
+function playGame(playerChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = '';
+
+    if (playerChoice === computerChoice) {
+        score.gamesPlayed += 1;
+        result = 'It\'s a tie!';
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        score.wins += 1;
+        score.gamesPlayed += 1;
+        result = 'You win!';
+    } else {
+        score.losses += 1;
+        score.gamesPlayed += 1;
+        result = 'You lose!';
+    }
+    document.querySelector('.status').innerHTML = result
+    document.querySelector('.choice').innerHTML = `You <img src="icons/${playerChoice}-emoji.png" class ="icons"> <img src="icons/${computerChoice}-emoji.png" class ="icons"> Computer `
+    document.querySelector('.score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.gamesPlayed - score.wins - score.losses}`
+}
+const score = {
+    gamesPlayed: 0,
+    wins: 0,
+    losses: 0
+};
+function resetScore(){
+    score.gamesPlayed=0;
+    score.wins=0;
+    score.losses=0;
+    document.querySelector('.status').innerHTML = 'Game Reset!';
+    document.querySelector('.choice').innerHTML = '';
+    document.querySelector('.score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.gamesPlayed - score.wins - score.losses}`;
+}
