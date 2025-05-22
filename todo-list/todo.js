e3b41e732be405a80d0e3d418f67059ede323502
PhamNhatQuang207 <phamquang207207@gmail.com>
2025-05-22 14:40:55 +0700

    finish todo list

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -3,23 +3,30 @@ renderTodoList();
 function renderTodoList(){
     let todoListHTML = '';
         for(let i=0; i<todoList.length; i++){
-                const todo = todoList[i];
+                const todoObject = todoList[i];
+                const { name, doDate } = todoObject;
                 const html = 
-                `<p>
-                    ${todo} 
-                    <button onclick="todoList.splice(${i},1);
-                    renderTodoList()" class="delete-button"
-                    ">Delete</button>
-                </p>`
+                   `<div>${name}</div> 
+                    <div>${doDate}</div> 
+                    <button onclick="
+                    todoList.splice(${i},1);
+                    renderTodoList()" 
+                    class="delete-button"
+                    ">Delete</button>`
                 todoListHTML += html;
             }
     document.querySelector('.list').innerHTML = todoListHTML;
     }
 function addTodo() {
-    const todoInput = document.querySelector('.todo-input');
-    const name = todoInput.value;
-    todoList.push(name);
-    todoInput.value = '';
+    const todoName = document.querySelector('.todo-name');
+    const todoDate = document.querySelector('.todo-date');
+    const name = todoName.value;
+    const date = todoDate.value;
+    todoList.push({
+        name : name,
+        doDate : date,
+    });
+    todoName.value = '';
+    todoDate.value = ''
     renderTodoList();
 }   
-f 
