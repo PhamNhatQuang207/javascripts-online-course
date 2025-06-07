6dd7a9ffc4a1c14fff527d786e2207a503ce3659
PhamNhatQuang207 <phamquang207207@gmail.com>
2025-06-07 15:00:59 +0700

    try using foreach

---
+++ todo-list/todo-advance.js
@@ -0,0 +1,31 @@
+const todoList = [];
+renderTodoList();
+function renderTodoList(){
+    let todoListHTML = '';
+        todoList.forEach(function(todoObject,i){
+        const { name, doDate } = todoObject;
+            const html = 
+            `<div>${name}</div> 
+            <div>${doDate}</div> 
+            <button onclick="
+            todoList.splice(${i},1);
+            renderTodoList()" 
+            class="delete-button"
+            ">Delete</button>`
+            todoListHTML += html;                    
+            });
+    document.querySelector('.list').innerHTML = todoListHTML;
+}
+function addTodo() {
+    const todoName = document.querySelector('.todo-name');
+    const todoDate = document.querySelector('.todo-date');
+    const name = todoName.value;
+    const date = todoDate.value;
+    todoList.push({
+        name : name,
+        doDate : date,
+    });
+    todoName.value = '';
+    todoDate.value = ''
+    renderTodoList();
+}   
