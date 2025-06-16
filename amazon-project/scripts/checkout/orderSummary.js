3c2f421b7471d8451eb4df7ec74c6d96b7ba2cfc
PhamNhatQuang207 <phamquang207207@gmail.com>
2025-06-16 20:42:58 +0700

    render payment

--- amazon-project/scripts/checkout/orderSummary.js
+++ amazon-project/scripts/checkout/orderSummary.js
@@ -2,7 +2,7 @@ import { cart,removeFromCart,removeProduct,updateDeliveryOption } from "../../da
 import { products,getProduct } from "../../data/products.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
 import { deliveryOptions,getDeliveryOption } from "../../data/deliveryOptions.js";
-
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary(){
     let cartSummaryHTML = ''
@@ -98,6 +98,7 @@ export function renderOrderSummary(){
             removeFromCart(productId);
             const container =document.querySelector(`.cart-item-container-${productId}`);
             container.remove();
+            renderPaymentSummary();
         });
     })
 
@@ -106,6 +107,7 @@ export function renderOrderSummary(){
             const {productId,deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 }
