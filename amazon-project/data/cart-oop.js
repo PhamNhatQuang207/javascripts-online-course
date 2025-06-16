125dc6dbab2ed932dea68b542c1782a7cea19de5
PhamNhatQuang207 <phamquang207207@gmail.com>
2025-06-16 21:41:57 +0700

    change localKey and loadFromStorage to private

--- amazon-project/data/cart-oop.js
+++ amazon-project/data/cart-oop.js
@@ -1,13 +1,13 @@
 class Cart{
     cartItems;
-    localStorageKey;
+    #localStorageKey;
     
     constructor(localStorageKey){
-        this.localStorageKey=localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey=localStorageKey;
+        this.#loadFromStorage();
     }
-    loadFromStorage(){
-            this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    #loadFromStorage(){
+            this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
             if (!this.cartItems){
                 this.cartItems = [{
                     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -22,7 +22,7 @@ class Cart{
         }
 
     saveToStorage() {
-        localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartItems));
+        localStorage.setItem(this.#localStorageKey,JSON.stringify(this.cartItems));
     }
 
     addToCart(productId){
@@ -79,11 +79,6 @@ class Cart{
 
 const cart = new Cart('cart');
 const businessCart = new Cart('businessCart');
-
-
-cart.loadFromStorage();
-
-businessCart.loadFromStorage();
 
 console.log(cart);
 console.log(businessCart)
