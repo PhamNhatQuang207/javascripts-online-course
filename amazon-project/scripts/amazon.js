64d9c53a9e667a4e51b3d76c5912fd8ed8bce5c4
PhamNhatQuang207 <phamquang207207@gmail.com>
2025-06-16 22:01:32 +0700

    change product to object and add some method

--- amazon-project/scripts/amazon.js
+++ amazon-project/scripts/amazon.js
@@ -17,14 +17,14 @@ products.forEach((products) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${products.rating.stars * 10}.png">
+              src="${products.getStarsURL()}">
             <div class="product-rating-count link-primary">
               ${products.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            ${(products.priceCents / 100).toFixed(2)}
+            ${products.getPrice()}
           </div>
 
           <div class="product-quantity-container">
