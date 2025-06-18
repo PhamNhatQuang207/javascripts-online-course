126cd3eb5144cd0474c41b755090fba4ad77da71
PhamNhatQuang207 <phamquang207207@gmail.com>
2025-06-18 14:39:36 +0700

    using async and await

--- amazon-project/scripts/checkout.js
+++ amazon-project/scripts/checkout.js
@@ -2,10 +2,15 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import  {loadProducts,loadProductsFetch} from "../data/products.js"
 import '../data/cart-oop.js';
-import { loadCart } from "../data/cart-oop.js";
-//import '../data/backend.js';
 
-Promise.all([
+async function loadPage(){
+    await loadProductsFetch();
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+loadPage();
+
+/*Promise.all([
     loadProductsFetch(),
     new Promise((resolve)=>{
         loadCart(()=>{
